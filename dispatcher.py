c92b9f8db6b083228fc65cebe236940a1b5192aa
jcbain <jamescbain@gmail.com>
2019-09-12 11:05:18 -0600

    write and update documentation

--- dispatcher.py
+++ dispatcher.py
@@ -57,15 +57,28 @@ def parse_output(output, rep):
 
 def create_params(param_str, *opts):
     """
+    Creates a list of explicit simulation parameter options for a single parameter.
 
     Parameters
     ----------
-    param_str
-    opts
+    param_str: str
+       A string parameter option to be modified which will generally take on the for of "<param>=". For example, to
+       specify the rate of migration (m) you would input "m=".
+    opts: str
+       Any number of parameter values that you wish to run for any given parameter. For example, if you wanted to run a
+       simulation with migrations rates of 1e-5 and 1e-3 then you would pass `"1e-5", "1e-3"` as arguments after the
+       `param_str` option.
 
     Returns
     -------
+    list
+        A list of strings explicitly specifying the parameter to be run within the slim script.
 
+    Examples
+    --------
+    To specify a simulation that runs migration rates (m) set to 1-e5 and 1-e4 you would:
+    >>> create_params("m=", "1e-5", "1e-3")
+    ["m=1e-5", "m=1e-6"]
     """
     return [param_str + "{}".format(opt) for opt in opts]
 
@@ -73,8 +86,8 @@ def create_params(param_str, *opts):
 def main():
     """
     Provides a wrapper around the slim command and runs a file for simulating local adaptation called
-        `local_adaptation.slim`. This file can be run from the command line with the falling named arguments:
-        --rep : The number of replicates you want to run.
+    `local_adaptation.slim`. This file can be run from the command line with the falling named arguments:
+    --rep : The number of replicates you want to run.
 
     Returns:
         A call to the slim command running the script `local_adaptation.slim`
