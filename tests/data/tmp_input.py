4779fd422ada6f161865792149ec013cc4b4e23b
jcbain <jamescbain@gmail.com>
2019-10-14 12:58:53 -0600

    fix tests with new header file

--- tests/data/tmp_input.py
+++ tests/data/tmp_input.py
@@ -16,17 +16,17 @@ initializeRecombinationRate(1e-05);
 // Starting run at generation <start>:
 1 
 
-"position select_coef p1_freq p2_freq fixed_since migr_rate mut_rate recomb_rate fitness_width output_gen 
+"position select_coef p1_freq p2_freq orig_gen migr_rate mut_rate recomb_rate fitness_width output_gen 
 20001 -0.020159 0 283 370 1e-05 1e-05 1e-05 5 500
 20001 0.0116518 20 0 468 1e-05 1e-05 1e-05 5 500
 "
-"position select_coef p1_freq p2_freq fixed_since migr_rate mut_rate recomb_rate fitness_width output_gen 
+"position select_coef p1_freq p2_freq orig_gen migr_rate mut_rate recomb_rate fitness_width output_gen 
 20001 -0.020159 0 23 370 1e-05 1e-05 1e-05 5 1000
 20002 0.13452 18 0 964 1e-05 1e-05 1e-05 5 1000
 27502 -0.00283611 0 61 477 1e-05 1e-05 1e-05 5 1000
 140001 -0.0744118 2 0 994 1e-05 1e-05 1e-05 5 1000
 "
-"position select_coef p1_freq p2_freq fixed_since migr_rate mut_rate recomb_rate fitness_width output_gen 
+"position select_coef p1_freq p2_freq orig_gen migr_rate mut_rate recomb_rate fitness_width output_gen 
 20001 -0.0191989 1 0 1499 1e-05 1e-05 1e-05 5 1500
 20002 0.0196835 1 0 1500 1e-05 1e-05 1e-05 5 1500
 "
