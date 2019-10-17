5f14a1b7275623525e208b7284cb0f647dfc1686
jcbain <jamescbain@gmail.com>
2019-10-17 10:59:11 -0600

    update tests

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -1,5 +1,5 @@
 from context import dispatcher
-from tests.data.tmp_input import tmp_string
+from tests.data.tmp_input2 import tmp_string2
 
 
 class TestRemoveAll:
@@ -18,15 +18,15 @@ class TestRemoveAll:
 class TestParseOutput:
 
     def test_data_length(self):
-        data_elements = dispatcher.parse_output(tmp_string, 0)[0]
-        assert len(data_elements) == 8
+        data_elements = dispatcher.parse_output(tmp_string2, 'm39', 0)[0]
+        assert len(data_elements) == 6
 
     def test_data_extraction(self):
-        data_elements = dispatcher.parse_output(tmp_string, 1)[0]
-        assert data_elements[0] == '20001 -0.020159 0 283 370 1e-05 1e-05 1e-05 5 500 1'
+        data_elements = dispatcher.parse_output(tmp_string2, 'm39', 1)[0]
+        assert data_elements[0] == '170001 0.0327583 0 0.0005 3997 1e-10 1e-05 1e-06 2 4000 1'
 
     def test_header_configuration(self):
-        header_element = dispatcher.parse_output(tmp_string, 0)[1]
-        assert header_element == 'position select_coef p1_freq p2_freq orig_gen migr_rate mut_rate recomb_rate ' \
-                                 'fitness_width output_gen '
+        header_element = dispatcher.parse_output(tmp_string2, 'm39', 0)[1]
+        assert header_element == 'position select_coef p1_freq p2_freq origin_gen migr_rate mut_rate recomb_rate ' \
+                                 'fitness_width output_gen'
 
